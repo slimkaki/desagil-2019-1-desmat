20e9c72f6dc19c1825febdf0d9de9d81852de7af
Marcompp <marcomoliternop@gmail.com>
2019-04-01 16:46:04 -0300

    CFCCalculator

---
+++ src/test/java/br/pro/hashi/ensino/desagil/desmat/model/CFCCalculatorTest.java
@@ -0,0 +1,19 @@
+package br.pro.hashi.ensino.desagil.desmat.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CFCCalculatorTest {
+    private Calculator calculator;
+
+    @BeforeEach
+    public void setUp() {
+        calculator = new CFCCalculator();
+    }
+
+    @Test
+    public void testeCesio() {
+        Assertions.assertEquals(1.92, calculator.calculate(40.08, 0.197), 0.000001);
+    }
+}
